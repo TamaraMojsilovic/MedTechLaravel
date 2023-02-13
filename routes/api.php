71a9900f772f6bcc14a091818d621716ea4afe2e
Tamara Mojsilovic <mojsilovictamara324@gmail.com>
2023-02-13 02:57:44 +0100

    poslednje promene

--- routes/api.php
+++ routes/api.php
@@ -17,18 +17,15 @@ use App\Http\Controllers\PacijentController;
 |
 */
 
-
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+Route::get('lekar', [LekarController::class, 'index']);
+Route::get('lekar/{lekar}', [LekarController::class, 'show']);
+Route::get('pacijent', [PacijentController::class, 'index']);
+Route::get('pacijent/{pacijent}', [PacijentController::class, 'show']);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::get('lekar', [LekarController::class, 'index']);
-    Route::get('lekar/{lekar}', [LekarController::class, 'show']);
     Route::delete('lekar/{lekar}', [LekarController::class, 'destroy']);
-
-    Route::get('pacijent', [PacijentController::class, 'index']);
-    Route::get('pacijent/{pacijent}', [PacijentController::class, 'show']);
     Route::post('pacijent', [PacijentController::class, 'store']);
-
     Route::post('logout', [AuthController::class, 'logout']);
 });
